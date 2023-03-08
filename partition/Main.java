91b88696e4633b5a89e8b107fa1010f64553ce8a
Rachid-Mek <78279178+Rachid-Mek@users.noreply.github.com>
2023-03-08 14:56:32 +0100

    Update Main.java

--- partition/Main.java
+++ partition/Main.java
@@ -1,10 +1,10 @@
-package partition;
+package partition.notExactSol;
 import java.util.Arrays;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = Partition.generateArray(10,100);
+        int[] arr = Partition.generateArray(5,100);
         System.out.println("Array : " + Arrays.toString(arr)); // to string pour afficher le tableau sous form de chaine de caractere
         List<Integer>[] partition = Partition.partitionement(arr);
         System.out.println("S1 = " + partition[0]);
