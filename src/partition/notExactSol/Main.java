6ef6ec11bd5b2d0294edbdb93f0082528875fb8f
Rachid <rachidmekdoud@gmail.com>
2023-03-10 20:53:27 +0100

    updated

--- src/partition/notExactSol/Main.java
+++ src/partition/notExactSol/Main.java
@@ -4,7 +4,8 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = Partition.generateArray(5,100);
+        int[] arr ={ 382745, 799601, 909247, 729069, 467902, 44328, 34610, 698150, 823460, 903959, 853665, 551830, 610856,
+                670702, 488960, 951111, 323046, 446298, 931161, 31385, 496951, 264724, 224916, 169684 } ;
         System.out.println("Array : " + Arrays.toString(arr)); // to string pour afficher le tableau sous form de chaine de caractere
         List<Integer>[] partition = Partition.partitionement(arr);
         System.out.println("S1 = " + partition[0]);
