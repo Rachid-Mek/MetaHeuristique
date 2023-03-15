acb2ccef81703f10d5c46e2ad4dbef675551fd69
Rachid <rachidmekdoud@gmail.com>
2023-03-15 13:35:09 +0100

    Added heuristic and cost functions

---
+++ src/partition/heuristique/PartitionAStar.java
@@ -0,0 +1,28 @@
+package partition.heuristique;
+import java.util.*;
+
+public class PartitionAStar {
+
+    public static int depthCost(ArrayList<Integer> S1) {
+        return S1.size();
+    }
+    //la somme minimale nécessaire pour atteindre la solution
+    public static int minSumHeuristic(ArrayList<Integer> S, ArrayList<Integer> S1) {
+        int sumS1 = 0;
+        for (int i : S1) {
+            sumS1 += i;
+        }
+        int sumS2 = 0;
+        for (int i : S) {
+            if (!S1.contains(i)) {
+                sumS2 += i;
+            }
+        }
+        return Math.abs(sumS1 - sumS2);
+    }
+
+    public static int g(int[] P, int i, int sum) {
+        return sum;
+    }
+
+}
