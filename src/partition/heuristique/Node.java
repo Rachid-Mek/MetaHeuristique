91c39ca927c36d913f282dfc268dd2c99a60f5f7
Rachid <rachidmekdoud@gmail.com>
2023-03-19 20:12:03 +0100

    Added Node

---
+++ src/partition/heuristique/Node.java
@@ -0,0 +1,13 @@
+package partition.heuristique;
+
+import java.util.ArrayList;
+
+public class Node {
+    ArrayList<Integer> s1;
+    int f;
+
+    public Node(ArrayList<Integer> s1, int f) {
+        this.s1 = s1;
+        this.f = f;
+    }
+}
