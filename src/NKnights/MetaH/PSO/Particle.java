adbcb3b7cfa00a5e02f1c24da3026ccbc25952e8
Rachid <rachidmekdoud@gmail.com>
2023-04-28 20:39:08 +0100

    PSO done

--- src/NKnights/MetaH/PSO/Particle.java
+++ src/NKnights/MetaH/PSO/Particle.java
@@ -1,38 +1,74 @@
 package NKnights.MetaH.PSO;
-import java.util.ArrayList;
+
+import java.util.Random;
 
 public class Particle {
-    private ArrayList<Integer> position; // particle position (queens' positions)
-    private ArrayList<Integer> velocity; // particle velocity
-    private int fitness; // particle fitness value
+    private int[] position; // Position de la particule
+    private int[] bestPosition; // Meilleure position trouvée par la particule (Pbest)
+    private int fitness; // Fitness de la position actuelle de la particule
+    private int bestFitness; // Meilleure fitness trouvée par la particule
+    private double[] velocity; // Vitesse de la particule
 
-    public Particle(ArrayList<Integer> position, ArrayList<Integer> velocity, int fitness) {
-        this.position = position;
-        this.velocity = velocity;
-        this.fitness = fitness;
+    public Particle(int n) {
+        this.position = new int[n];
+        this.bestPosition = new int[n];
+        this.fitness = Integer.MAX_VALUE;
+        this.bestFitness = Integer.MAX_VALUE;
+        this.velocity = new double[n];
+        Random rand = new Random();
+        for (int i = 0; i < n; i++) {
+            position[i] = rand.nextInt(n);
+            velocity[i] = rand.nextDouble(n);
+        }
     }
 
-    public ArrayList<Integer> getPosition() {
+    // Retourne la position de la particule
+    public int[] getPosition() {
         return position;
     }
 
-    public void setPosition(ArrayList<Integer> position) {
+    // Modifie la position de la particule
+    public void setPosition(int[] position) {
         this.position = position;
     }
 
-    public ArrayList<Integer> getVelocity() {
-        return velocity;
+    // Retourne la meilleure position trouvée par la particule
+    public int[] getBestPosition() {
+        return bestPosition;
     }
 
-    public void setVelocity(ArrayList<Integer> velocity) {
-        this.velocity = velocity;
+    // Modifie la meilleure position trouvée par la particule
+    public void setBestPosition(int[] bestPosition) {
+        this.bestPosition = bestPosition;
     }
 
+    // Retourne la fitness de la position actuelle de la particule
     public int getFitness() {
         return fitness;
     }
 
+    // Modifie la fitness de la position actuelle de la particule
     public void setFitness(int fitness) {
         this.fitness = fitness;
+    }
+
+    // Retourne la meilleure fitness trouvée par la particule
+    public int getBestFitness() {
+        return bestFitness;
+    }
+
+    // Modifie la meilleure fitness trouvée par la particule
+    public void setBestFitness(int bestFitness) {
+        this.bestFitness = bestFitness;
+    }
+
+    // Retourne la vitesse de la particule
+    public double[] getVelocity() {
+        return velocity;
+    }
+
+    // Modifie la vitesse de la particule
+    public void setVelocity(double[] velocity) {
+        this.velocity = velocity;
     }
 }
--- src/NKnights/MetaH/PSO/Pso.java
+++ src/NKnights/MetaH/PSO/Pso.java
@@ -2,168 +2,248 @@ package NKnights.MetaH.PSO;
 
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.Arrays;
 
 public class Pso {
-    int n; // number of queens
-    int m; // number of particles
-    int maxIterations; // maximum number of iterations
-    int maxVelocity; // maximum velocity
-    int w; // inertia weight
-    int c1; // cognitive weight
-    int c2; // social weight
+    private static int n; // Taille du problème
+    private int nbIterations; // Nombre d'itérations à effectuer
+    private int nbParticles; // Nombre de particules
+    private double c1; // Facteur d'attraction de la particule envers sa propre meilleure position
+    private double c2; // Facteur d'attraction de la particule envers la meilleure position globale
+    private double w; // Facteur d'inertie
+    private int[] bestPosition; // Meilleure position globale trouvée
+    private int bestFitness; // Meilleure fitness trouvée
+    private ArrayList<Particle> swarm; // Ensemble des particules
+    private Random rand; // Générateur de nombres aléatoires
 
-    public Pso(int n, int m, int maxIterations, int maxVelocity, int w, int c1, int c2) {
-        this.n = n;
-        this.m = m;
-        this.maxIterations = maxIterations;
-        this.maxVelocity = maxVelocity;
-        this.w = w;
+    public Pso(int n, int nbIterations, int nbParticles, double c1, double c2, double w) {
+        Pso.n = n;
+        this.nbIterations = nbIterations;
+        this.nbParticles = nbParticles;
         this.c1 = c1;
         this.c2 = c2;
+        this.w = w;
+        this.bestPosition = new int[n];
+        this.bestFitness = Integer.MAX_VALUE;
+        this.swarm = new ArrayList<Particle>();
+        this.rand = new Random();
     }
 
-    private int fitness(ArrayList<Integer> sol) {
-        int conflit = 0;
-        for (int i = 0; i < sol.size(); i++) {
-            for (int j = i + 1; j < sol.size(); j++) {
-                if (sol.get(i) == sol.get(j)) {
-                    conflit++;
+    // Initialise la position des particules aléatoirement
+    public void initializeSwarm() {
+        for (int i = 0; i < nbParticles; i++) {
+            Particle p = new Particle(n);
+            swarm.add(p);
+            // Mise à jour des paramètres de la particule
+            p.setFitness(computeFitness(p.getPosition()));
+            p.setBestPosition(p.getPosition().clone());
+            p.setBestFitness(p.getFitness());
+        }
+    }
+
+    // Évaluation de fitness (nombre de conflits) d'une position
+    public static int computeFitness(int[] position) {
+        int fitness = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (position[i] == position[j] || Math.abs(position[i] - position[j]) == j - i) {
+                    fitness++;
                 }
-                else if (Math.abs(sol.get(i) - sol.get(j)) == Math.abs(i - j)) {
-                    conflit++;
-                }
             }
         }
-        return conflit;
+        return fitness;
     }
 
-    public ArrayList<Integer> generateRandomSolution() {
-        ArrayList<Integer> solution = new ArrayList<Integer>();
-        Random rand = new Random();
-        for (int i = 0; i < n; i++) {
-            solution.add(rand.nextInt(n)); // add a random number between 0 and n-1
-        }
-        return solution;
-    }
-    private ArrayList<Integer> generateRandomVelocity() {
-        ArrayList<Integer> vel = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            vel.add((int) (Math.random() * maxVelocity));
-        }
-        return vel;
-    }
-
-    public ArrayList<Particle> generateSwarm() {
-        ArrayList<Particle> swarm = new ArrayList<>();
-        for (int i = 0; i < m; i++) {
-            ArrayList<Integer> position = generateRandomSolution();
-            ArrayList<Integer> velocity = generateRandomVelocity();
-            int fitness = fitness(position);
-            swarm.add(new Particle(position, velocity, fitness));
-        }
-        return swarm;
-    }
-
-    public Particle getGlobalBest(ArrayList<Particle> swarm) {
-        Particle globalBest = swarm.get(0);
-        for (int i = 1; i < swarm.size(); i++) {
-            Particle particle = swarm.get(i);
-            if (particle.getFitness() < globalBest.getFitness()) {
-                globalBest = particle;
+    // Met à jour la meilleure position globale trouvée
+    public void updateBestPosition() {
+        for (Particle p : swarm) {
+            if (p.getBestFitness() < bestFitness) {
+                bestFitness = p.getBestFitness();
+                bestPosition = p.getBestPosition().clone();
             }
         }
-        return globalBest;
     }
 
-    private void updateVelocity(Particle p, ArrayList<Integer> globalBest) {
-        ArrayList<Integer> newVelocity = new ArrayList<>();
-        ArrayList<Integer> currentPosition = p.getPosition();
-        ArrayList<Integer> currentVelocity = p.getVelocity();
+    public void updateVelocity(Particle p) {
+        double[] newVelocity = new double[n];
+        double r1 = rand.nextDouble();
+        double r2 = rand.nextDouble();
         for (int i = 0; i < n; i++) {
-            int personalBest = p.getPosition().get(i);
-            int globalBestPosition = globalBest.get(i);
-            int currentPositionValue = currentPosition.get(i);
-            int currentVelocityValue = currentVelocity.get(i);
-            int r1 = (int) (Math.random() * 10);
-            int r2 = (int) (Math.random() * 10);
-            int newVelocityValue = (int) (w * currentVelocityValue
-                    + c1 * r1 * (personalBest - currentPositionValue)
-                    + c2 * r2 * (globalBestPosition - currentPositionValue));
-
-            if (newVelocityValue > maxVelocity) {
-                newVelocityValue = maxVelocity;
-            } else if (newVelocityValue < -maxVelocity) {
-                newVelocityValue = -maxVelocity;
-            }
-            newVelocity.add(newVelocityValue);
+            double cognitiveComponent = c1 * r1 * (p.getBestPosition()[i] - p.getPosition()[i]);
+            double socialComponent = c2 * r2 * (bestPosition[i] - p.getPosition()[i]);
+            newVelocity[i] = w * p.getVelocity()[i] + cognitiveComponent + socialComponent;
         }
         p.setVelocity(newVelocity);
     }
 
-    //update position
-    private void updatePosition(Particle p) {
-        ArrayList<Integer> newPosition = new ArrayList<>();
-        ArrayList<Integer> currentPosition = p.getPosition();
-        ArrayList<Integer> currentVelocity = p.getVelocity();
+    public void updatePosition(Particle p) {
+        int[] newPosition = new int[n];
         for (int i = 0; i < n; i++) {
-            int newPositionValue = (currentPosition.get(i) + currentVelocity.get(i)) % n;
-            if (newPositionValue < 0) {
-                newPositionValue = n + newPositionValue;
-            }
-            newPosition.add(newPositionValue);
+            newPosition[i] = (int) Math.round(p.getPosition()[i] + p.getVelocity()[i]);
         }
+            for (int i = 0; i < n; i++) {
+                if (newPosition[i] < 0) {
+                    newPosition[i] = 0;
+                } else if (newPosition[i] > n - 1) {
+                    newPosition[i] = n - 1;
+                }
+            }
+            hillClimbing(p);
+}
+    private void hillClimbing(Particle p){
+        int[] newPosition = Arrays.copyOf(p.getPosition(), n);
+        int currentFitness = computeFitness(newPosition);
+        int bestFitness = currentFitness;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (j == newPosition[i]) continue;
+
+                int[] neighborPosition = Arrays.copyOf(newPosition, n);
+                neighborPosition[i] = j;
+                int neighborFitness = computeFitness(neighborPosition);
+
+                if (neighborFitness < bestFitness) {
+                    bestFitness = neighborFitness;
+                    newPosition = neighborPosition;
+                }
+            }
+        }
+        // Update the position of the particle
         p.setPosition(newPosition);
-    }
-    //update fitness
-    private void updateFitness(Particle p) {
-        int newFitness = fitness(p.getPosition());
-        p.setFitness(newFitness);
-    }
-    //update particle
-    private void updateParticle(Particle p, ArrayList<Integer> globalBest) {
-        updateVelocity(p, globalBest);
-        updatePosition(p);
-        updateFitness(p);
-    }
-    //update swarm
-    private void updateSwarm(ArrayList<Particle> swarm, ArrayList<Integer> globalBest) {
-        for (Particle particle : swarm) {
-            updateParticle(particle, globalBest);
+        // Update the best position of the particle
+        if (bestFitness < p.getBestFitness()) {
+            p.setBestFitness(bestFitness);
+            p.setBestPosition(newPosition.clone());
         }
     }
-    //solve
-    public ArrayList<Integer> solve() {
-        ArrayList<Particle> swarm = generateSwarm();
-        ArrayList<Integer> globalBest = getGlobalBest(swarm).getPosition();
-        for (int i = 0; i < maxIterations; i++) {
-            updateSwarm(swarm, globalBest);
-            ArrayList<Integer> currentGlobalBest = getGlobalBest(swarm).getPosition();
-            if (fitness(currentGlobalBest) < fitness(globalBest)) {
-                globalBest = currentGlobalBest;
+
+    // Exécute l'algorithme PSO
+    public void solvePso() {
+        // Initialize the swarm
+        initializeSwarm();
+
+        // Main loop of the algorithm
+        for (int i = 0; i < nbIterations; i++) {
+            // Update the best position found so far (global best)
+            updateBestPosition();
+            // Update the position and velocity of each particle
+            for (Particle p : swarm) {
+                // Solution found
+                if (Pso.CountConflict(p.getBestPosition()) == 0) {
+                    System.out.println("Solution found: " + Arrays.toString(bestPosition));
+                    System.out.println("Conflicts: " + Pso.CountConflict(bestPosition));
+                    return;
+                }
+                // Update the velocity of the particle
+                 updateVelocity(p);
+
+                //update the position of the particle
+                updatePosition(p);
+                // si la nouvelle position est meilleure que la meilleure position de la particule
+               if(bestFitness < p.getBestFitness()){
+                   p.setBestFitness(bestFitness);
+                   p.setBestPosition(p.getPosition().clone());
+               }
+            }
+            System.out.println("Iteration " + i + " best fitness: " + bestFitness);
+        }
+    }
+    // Retourne la meilleure fitness trouvée
+    public int getBestFitness() {
+        return bestFitness;
+    }
+
+    public static void performSensitivityAnalysis(int boardSize) {
+        // Set the parameter ranges to explore
+        int[] numIterations = {100, 500, 1000, 5000};
+        int[] numParticles = {20, 50, 100};
+        double[] c1 = {1.0, 1.5, 2.0};
+        double[] c2 = {1.0, 1.5, 2.0};
+        double[] w = {0.4, 0.7, 1.0};
+
+        // Initialize the best fitness and corresponding parameters
+        double bestFitness = Double.POSITIVE_INFINITY;
+        int bestIterations = 0;
+        int bestParticles = 0;
+        double bestC1 = 0.0;
+        double bestC2 = 0.0;
+        double bestW = 0.0;
+
+        // Loop over all parameter combinations
+        for (int i = 0; i < numIterations.length; i++) {
+            for (int j = 0; j < numParticles.length; j++) {
+                for (int k = 0; k < c1.length; k++) {
+                    for (int l = 0; l < c2.length; l++) {
+                        for (int m = 0; m < w.length; m++) {
+                            // Create a new PSO instance with the current parameters
+                            Pso pso = new Pso(boardSize, numIterations[i], numParticles[j], c1[k], c2[l], w[m]);
+
+                            // Run the PSO algorithm
+                            pso.solvePso();
+
+                            // Check if the current solution is better than the previous best
+                            if (pso.getBestFitness() < bestFitness) {
+                                bestFitness = pso.getBestFitness();
+                                bestIterations = numIterations[i];
+                                bestParticles = numParticles[j];
+                                bestC1 = c1[k];
+                                bestC2 = c2[l];
+                                bestW = w[m];
+                            }
+                        }
+                    }
+                }
             }
         }
-        return globalBest;
+
+        // Print the best parameters and corresponding fitness
+        System.out.println("Best parameters found: ");
+        System.out.println("Number of iterations: " + bestIterations);
+        System.out.println("Number of particles: " + bestParticles);
+        System.out.println("Acceleration constant c1: " + bestC1);
+        System.out.println("Acceleration constant c2: " + bestC2);
+        System.out.println("Inertia weight w: " + bestW);
+        System.out.println("Best fitness: " + bestFitness);
     }
+
 
     public static void main(String[] args) {
-        Pso pso = new Pso(10, 500, 10000, 3, 1, 2, 2);
-        ArrayList<Integer> solution = pso.solve();
-        System.out.println(solution);
-        System.out.println(pso.conflict(solution));
-    }
 
-    public int conflict(ArrayList<Integer> sol) {
-        int conflit = 0;
-        for (int i = 0; i < sol.size(); i++) {
-            for (int j = i + 1; j < sol.size(); j++) {
-                if (sol.get(i) == sol.get(j)) {
-                    conflit++;
+        // Set the problem parameters
+        int boardSize = 40;
+        int numIterations = 500;
+        int numParticles =300;
+        double c1 =2;
+        double c2 = 2;
+        double w = 0.9;
+
+
+        long startTime = System.currentTimeMillis();
+        // Create a PSO instance
+        Pso pso = new Pso(boardSize, numIterations, numParticles, c1, c2, w);
+        long endTime = System.currentTimeMillis();
+        long executionTime = endTime - startTime;
+
+
+        // Run the PSO algorithm
+        pso.solvePso();
+        System.out.println("Execution time: " + executionTime + " ms");
+
+    }
+    public static int CountConflict(int[] position) {
+        int conflict = 0;
+        for (int i = 0; i < position.length; i++) {
+            for (int j = i + 1; j < position.length; j++) {
+                if (position[i] == position[j]) {
+                    conflict++;
                 }
-                else if (Math.abs(sol.get(i) - sol.get(j)) == Math.abs(i - j)) {
-                    conflit++;
+                if (Math.abs(position[i] - position[j]) == Math.abs(i - j)) {
+                    conflict++;
                 }
             }
         }
-        return conflit;
+        return conflict;
     }
+
 }
